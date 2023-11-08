9351534bd2546fd8f3c9ea6aada9526cdfebc902
Mikołaj <niko.kosiorek@gmail.com>
2023-11-08 23:51:59 +0100

    AddedComponents

--- todo-list-theme/src/app/todo-task/todo-task.component.ts
+++ todo-list-theme/src/app/todo-task/todo-task.component.ts
@@ -6,11 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-task.component.css']
 })
 export class TodoTaskComponent {
-  Tasks: any = 
-  [
-    ['Important', 'Work', 'Finish Angular Proj'],
-    ['Very Important', 'Family', 'Order Pizza'],
-    ['Not Important', 'Home', 'Nigger']
+  Tasks: any = [
+    {priority: 'Important', category: 'Work', task: 'Finish Angular Proj'},
+    {priority: 'Very Important', category: 'Family', task: 'Order Pizza'},
+    {priority: 'Not Important', category: 'Home', task: 'Wash car'}
   ];
-
+  
+  AddTask(){
+    this.Tasks.push({priority: 'Not Important', category: 'Home', task: 'Wash car'})
+  }
+  
+  DeleteTask(task: string){
+    //this.Tasks = this.Tasks.filter(e => e !== task);
+  }
 }
